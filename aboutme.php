ede52485514fdf9c1c41dacfda356590affb4618
ChrisOwens505 <chrisowens132@gmail.com>
2017-10-09 16:34:51 -0600

    re-orginized my code

---
+++ aboutme.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+	<head>
+<meta charset="utf-8">
+		<title>about christian</title>
+	</head>
+	<body>
+		<h1>About christian</h1>
+		<p>
+			this is the website im making about <strong>myself</strong>
+		</p>
+		<div>
+			<h2>About me</h2>
+		</div>
+	</body>
+</html>
