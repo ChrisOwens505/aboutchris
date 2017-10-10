31dadfbb1505eabd3cdac857fe931013329b3020
ChrisOwens505 <chrisowens132@gmail.com>
2017-10-09 21:38:11 -0600

    added <h2> and list tags

--- aboutme.php
+++ aboutme.php
@@ -1,16 +1,31 @@
 <!DOCTYPE html>
 <html>
 	<head>
-<meta charset="utf-8">
-		<title>about christian</title>
+		<meta charset="utf-8">
+		<title>the story of christian</title>
 	</head>
 	<body>
 		<h1>About christian</h1>
 		<p>
-			this is the website im making about <strong>myself</strong>
+			This is the website about <strong>Christian owens</strong>
 		</p>
 		<div>
-			<h2>About me</h2>
+			<h2> little facts about me</h2>
+			<ul>
+				<li>born and raised in albuquerque new mexico</li>
+				<li>lived in new mexico his whole life</li>
+				<li>has been all over the country</li>
+				<li>loves puppies</li>
+			</ul>
 		</div>
+		<h2>important faces about me</h2>
+		<ul>
+			<li>i like to play video games</li>
+			<li>i love to play football</li>
+			<li> i enjoy target practice</li>
+			<li></li>
+		</ul>
+		<div>
+
 	</body>
 </html>
