df53ca623fd866a1419430d16d223a5f66c9ef3f
ChrisOwens505 <chrisowens132@gmail.com>
2017-10-09 21:55:09 -0600

    added more too

--- aboutme.php
+++ aboutme.php
@@ -1,29 +1,30 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 	<head>
 		<meta charset="utf-8">
 		<title>the story of christian</title>
+		<link rel="stylesheet" href="design.css"
 	</head>
 	<body>
-		<h1>About christian</h1>
-		<p>
+		<h1 class="center">About christian</h1>
+		<p class="center">
 			This is the website about <strong>Christian owens</strong>
 		</p>
-		<div>
+		<div class="center">
 			<h2> little facts about me</h2>
-			<ul>
-				<li>born and raised in albuquerque new mexico</li>
-				<li>lived in new mexico his whole life</li>
-				<li>has been all over the country</li>
-				<li>loves puppies</li>
+			<ul class="center-list">
+				<li class="center-list">born and raised in albuquerque new mexico</li>
+				<li class="center-list">lived in new mexico his whole life</li>
+				<li class="center-list">has been all over the country</li>
+				<li class="center-list">loves puppies</li>
 			</ul>
-		</div>
+		</div class="center">
 		<h2>important faces about me</h2>
 		<ul>
-			<li>i like to play video games</li>
-			<li>i love to play football</li>
-			<li> i enjoy target practice</li>
-			<li></li>
+			<li class="css">i like to play video games</li>
+			<li class="css">i love to play football</li>
+			<li class="css"> i enjoy target practice</li>
+			<li class="css"> i want to also work in the music industry</li>
 		</ul>
 		<div>
 
